c3a0873550d8c470e6fb9af6a45753cd2ba0c256
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2025-02-01 16:02:22 +0400

    fix access path bug

--- src/accessPath.mjs
+++ src/accessPath.mjs
@@ -1,16 +1,12 @@
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export const accessPath = (userPath) => {
   return new Promise((resolve, reject) => {
-    const PATH = path.resolve(__dirname, userPath);
+    const PATH = path.resolve(process.cwd(), userPath);
     fs.access(PATH, fs.constants.F_OK, (err) => {
       if (err) reject();
-      else resolve();
+      else resolve(userPath);
     });
   });
 }
