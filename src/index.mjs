e855e77470e929f13c61582c20b9031e83b1533d
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2025-02-02 21:24:53 +0400

    add decompress

--- src/index.mjs
+++ src/index.mjs
@@ -2,10 +2,12 @@ import { up } from './up.mjs';
 import { os } from './os.mjs';
 import { hash } from './hash.mjs';
 import { compress } from './compress.mjs';
+import { decompress } from './decompress.mjs';
 
 export {
   up,
   os,
   hash,
   compress,
+  decompress,
 };
