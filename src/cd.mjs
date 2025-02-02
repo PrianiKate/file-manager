12fca17a13c121ef0027edc7c7f7491ade6acc4a
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2025-02-02 21:44:49 +0400

    update cd command

--- src/cd.mjs
+++ src/cd.mjs
@@ -1,14 +1,17 @@
 import { accessPath } from './accessPath.mjs';
 
 export const cd = async (args) => {
-  const path = args[0];
+  let path = args[0];
   if (!path) {
     process.stdout.write('Invalid input\n');
     return;
   }
-  const pathExistsStatus = await accessPath(path);
-  const absolutePathExistsStatus = await accessPath(path, true);
-  if (!pathExistsStatus && !absolutePathExistsStatus) {
+  const currentDir = process.cwd();
+  if (!path.startsWith(currentDir)) {
+    path = `${currentDir}${path}`;
+  }
+  const pathExistsStatus = await accessPath(path, true);
+  if (!pathExistsStatus) {
     process.stdout.write('Operation failed\n');
     return;
   }
